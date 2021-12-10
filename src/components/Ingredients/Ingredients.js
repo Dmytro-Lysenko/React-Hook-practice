e5742358f359870893026fe5c657a0e3e2a38cb6
Dmytro Lysenko <crystaljoint@gmail.com>
2021-12-10 17:23:53 +0200

    adding starting code, add sending POST request and fetch Get request from firebase using UseEffect hook

---
+++ src/components/Ingredients/Ingredients.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from "react";
+
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
+
+function Ingredients() {
+  const [userIndgredients, setUserIngredients] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://react-app-81b61-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json"
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+  }, []);
+
+  const addIngredientHandler = (ingredient) => {
+    fetch(
+      "https://react-app-81b61-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json",
+      {
+        method: "POST",
+        body: JSON.stringify(ingredient),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        console.log(responseData.name);
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          {
+            id: responseData.name,
+            ...ingredient,
+          },
+        ]);
+      });
+  };
+
+  const removeIngredientHandler = (ingredientId) => {
+    setUserIngredients((prevIngredients) =>
+      prevIngredients.filter((ing) => ing.id !== ingredientId)
+    );
+  };
+
+  return (
+    <div className="App">
+      <IngredientForm onAddIngredient={addIngredientHandler} />
+
+      <section>
+        <Search />
+        <IngredientList
+          ingredients={userIndgredients}
+          onRemoveItem={removeIngredientHandler}
+        />
+      </section>
+    </div>
+  );
+}
+
+export default Ingredients;
