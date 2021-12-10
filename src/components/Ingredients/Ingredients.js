ea0abf957bd87bb3e7e3c2cea71df698138aa228
Dmytro Lysenko <crystaljoint@gmail.com>
2021-12-10 21:57:53 +0200

    replacing logic of adding and removing ingredients by useReducer hook

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,41 +1,40 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useReducer } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
+import ErrorModal from "../UI/ErrorModal";
 import Search from "./Search";
 
-function Ingredients() {
-  const [userIndgredients, setUserIngredients] = useState([]);
+const ingredientReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case "SET":
+      return action.ingredients;
+    case "ADD":
+      return [...currentIngredients, action.ingredient];
+    case "DELETE":
+      return currentIngredients.filter((ing) => ing.id !== action.id);
+    default:
+      throw new Error("Should not get here");
+  }
+};
 
-  ////We can not use this fetch cos we already use fetch ingrediens from search component
-  ////
-  // useEffect(() => {
-  //   fetch(
-  //     "https://react-app-81b61-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json"
-  //   )
-  //     .then((response) => response.json())
-  //     .then((responseData) => {
-  //       const loadedIngredients = [];
-  //       for (const key in responseData) {
-  //         loadedIngredients.push({
-  //           id: key,
-  //           title: responseData[key].title,
-  //           amount: responseData[key].amount,
-  //         });
-  //       }
-  //       setUserIngredients(loadedIngredients);
-  //     });
-  // }, []);
+function Ingredients() {
+  const [userIndgredients, dispatch] = useReducer(ingredientReducer, []);
+  // const [userIndgredients, setUserIngredients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     console.log("Rendering ingredients", userIndgredients);
   }, [userIndgredients]);
 
   const filteredIngredientsHandler = useCallback((filtredIngredients) => {
-    setUserIngredients(filtredIngredients);
+    // setUserIngredients(filtredIngredients);
+    dispatch({ type: "SET", ingredients: filtredIngredients });
   }, []);
 
   const addIngredientHandler = (ingredient) => {
+    setIsLoading(true);
     fetch(
       "https://react-app-81b61-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json",
       {
@@ -47,36 +46,60 @@ function Ingredients() {
       }
     )
       .then((response) => {
+        setIsLoading(false);
         return response.json();
       })
       .then((responseData) => {
         console.log(responseData.name);
-        setUserIngredients((prevIngredients) => [
-          ...prevIngredients,
-          {
-            id: responseData.name,
-            ...ingredient,
-          },
-        ]);
+        // setUserIngredients((prevIngredients) => [
+        //   ...prevIngredients,
+        //   {
+        //     id: responseData.name,
+        //     ...ingredient,
+        //   },
+        // ]);
+        dispatch({
+          type: "ADD",
+          ingredient: { id: responseData.name, ...ingredient },
+        });
+      })
+      .catch((error) => {
+        setError("Something went wrong");
+        setIsLoading(false);
       });
   };
 
   const removeIngredientHandler = (ingredientId) => {
+    setIsLoading(true);
     fetch(
       `https://react-app-81b61-default-rtdb.europe-west1.firebasedatabase.app/ingredients/${ingredientId}.json`,
       {
         method: "DELETE",
       }
-    ).then((response) => {
-      setUserIngredients((prevIngredients) =>
-        prevIngredients.filter((ing) => ing.id !== ingredientId)
-      );
-    });
+    )
+      .then((response) => {
+        setIsLoading(false);
+        // setUserIngredients((prevIngredients) =>
+        //   prevIngredients.filter((ing) => ing.id !== ingredientId)
+        // );
+        dispatch({ type: "DELETE", id: ingredientId });
+      })
+      .catch((error) => {
+        setError("Something went wrong");
+      });
+  };
+
+  const clearError = () => {
+    setError(null);
   };
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={isLoading}
+      />
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
