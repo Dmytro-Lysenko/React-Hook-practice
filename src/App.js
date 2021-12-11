c712f4a4fbcc0477d59e01b51a70924c15240502
Dmytro Lysenko <crystaljoint@gmail.com>
2021-12-11 09:36:29 +0200

    adding useContext hook in authentication

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { Fragment } from "react";
 
-import Ingredients from './components/Ingredients/Ingredients';
+import Auth from "./components/Auth";
+import Ingredients from "./components/Ingredients/Ingredients";
+import { AuthContext } from "./context/auth-context";
 
-const App = props => {
-  return <Ingredients />;
+const App = (props) => {
+  const authContext = useContext(AuthContext);
+  const toogleLoginHandler = () =>{
+    authContext.login()
+  }
+
+
+  let content = <Auth loginHandler={toogleLoginHandler} />;
+  if (authContext.isAuth) {
+    content = <Ingredients />;
+  }
+  return <Fragment>{content}</Fragment>;
 };
 
 export default App;
